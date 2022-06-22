7206f7d06b9b12b2eacaca0f8f8518370e12e034
James John McGuire <jamesjohnmcguire@gmail.com>
2022-06-22 16:00:50 +0900

    Add setUpBeforeClass with arguments support

--- tests/UnitTests.php
+++ tests/UnitTests.php
@@ -10,8 +10,18 @@ use PHPUnit\Framework\TestCase;
 
 final class UnitTests extends TestCase
 {
+	protected static ?string $credentialsFilePath = null;
+
 	public function setUp() : void
 	{
+	}
+
+	public static function setUpBeforeClass() : void
+	{
+		if (!empty($argv[4]))
+		{
+			self::$credentialsFilePath = $argv[4];
+		}
 	}
 
 	public function tearDown() : void
@@ -26,6 +36,19 @@ final class UnitTests extends TestCase
 	}
 
 	function testTokensFailNoCredentials()
+	{
+		$client = GoogleAuthorization::Authorize(
+			Mode::Token,
+			self::$credentialsFilePath,
+			null,
+			null,
+			'Google Drive API File Uploader',
+			['https://www.googleapis.com/auth/drive']);
+	
+		$this->assertNull($client);
+	}
+
+	function testTokensFailNoTokens()
 	{
 		$client = GoogleAuthorization::Authorize(
 			Mode::Token,
