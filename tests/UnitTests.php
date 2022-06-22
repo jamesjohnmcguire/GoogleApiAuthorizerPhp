16b9d5b68e02efd29f4c28730c7307012f790b47
James John McGuire <jamesjohnmcguire@gmail.com>
2022-06-22 12:42:17 +0900

    Add essential use namespaces

--- tests/UnitTests.php
+++ tests/UnitTests.php
@@ -4,6 +4,8 @@ namespace GoogleAuthorization\Tests;
 require_once 'vendor/autoload.php';
 require_once 'GoogleAuthorization.php';
 
+use GoogleApiAuthorization\GoogleAuthorization;
+use GoogleApiAuthorization\Mode;
 use PHPUnit\Framework\TestCase;
 
 final class UnitTests extends TestCase
